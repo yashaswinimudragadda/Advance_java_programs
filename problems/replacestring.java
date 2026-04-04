36b96df4eedd64ade29af388f353d5f104dc5d88
Yashaswini Mudragadda <mudragaddarajaniyashaswini@gmail.com>
2026-04-04 15:02:01 +0530

    Create replacestring.java

---
+++ problems/replacestring.java
@@ -0,0 +1,13 @@
+public class main{
+    public static void main(String[ ] args){
+        String original = "i love doing job";
+        String target = "job";
+        String replacement ="high payment";
+        
+        String result = original.replace(target,replacement);
+        System.out.println("original: "+original);
+        System.out.println("Modified: "+result);
+        
+        
+    }
+}
